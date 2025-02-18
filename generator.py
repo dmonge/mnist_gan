bd15a5f70346b9f98045e15f20ebab3487677c9c
David A. Monge <damonge27@gmail.com>
2025-02-18 12:17:22 +0100

    new generator architecture

--- generator.py
+++ generator.py
@@ -11,20 +11,33 @@ class Generator(nn.Module):
         super().__init__()
         self.hidden_size = hidden_size
         self.img_size = img_size
-        self.fc = nn.Linear(latent_size + n_classes, hidden_size * img_size // 4 * img_size // 4)
+        self.input = self.input_block(latent_size, n_classes, hidden_size, img_size)
         self.deconv1 = self.deconv_block(hidden_size, hidden_size // 2)
-        self.deconv2 = self.deconv_block(hidden_size // 2, 1)
+        self.deconv2 = self.deconv_block(hidden_size // 2, hidden_size // 4)
+        self.deconv3 = nn.ConvTranspose2d(hidden_size // 4, 1, kernel_size=3, stride=1, padding=1)
+
+    def input_block(self, latent_size, n_classes, hidden_size, img_size):
+        in_features = latent_size + n_classes
+        out_featues = hidden_size * img_size // 4 * img_size // 4
+        return nn.Sequential(
+            nn.Linear(in_features, out_featues),
+            nn.BatchNorm1d(out_featues),
+            nn.ReLU(),
+        )
 
     def deconv_block(self, in_filters, out_filters):
         return nn.Sequential(
-            nn.BatchNorm2d(in_filters),
+            nn.ConvTranspose2d(in_filters, out_filters, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(out_filters),
             nn.ReLU(),
-            nn.ConvTranspose2d(in_filters, out_filters, kernel_size=4, stride=2, padding=1)
         )
 
     def forward(self, x):
-        x = self.fc(x)
+        x = self.input(x)
         x = x.view(x.shape[0], self.hidden_size, self.img_size // 4, self.img_size // 4)
         x = self.deconv1(x)
         x = self.deconv2(x)
-        return F.sigmoid(x)
+        x = self.deconv3(x)
+        return F.tanh(x)
+
+
