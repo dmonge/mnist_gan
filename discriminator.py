4ca64d309ddb012507c95af378389e6004ef89db
David A. Monge <damonge27@gmail.com>
2025-02-18 12:17:43 +0100

    new discriminator architecture

--- discriminator.py
+++ discriminator.py
@@ -1,8 +1,8 @@
 """
 Discriminator.
 """
+import torch
 from torch import nn
-from torch.nn import functional as F
 
 
 REAL_IMAGE = 1.0
@@ -15,20 +15,42 @@ class Discriminator(nn.Module):
         super().__init__()
         self.conv1 = self.conv_block(1, hidden_size // 2)
         self.conv2 = self.conv_block(hidden_size // 2, hidden_size)
-        self.fc = nn.Linear(hidden_size * img_size // 4 * img_size // 4, 1)
+        self.fc1 = nn.utils.spectral_norm(nn.Linear(hidden_size * img_size // 4 * img_size // 4, 100))
+        self.disc = MinibatchDiscrimination(100, 50, 5)
+        self.fc2 = nn.Linear(100 + 50, 1)
 
     def conv_block(self, in_filters, out_filters):
         return nn.Sequential(
-            nn.BatchNorm2d(in_filters),
-            nn.ReLU(),
-            nn.Conv2d(in_filters, out_filters, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
+            nn.utils.spectral_norm(nn.Conv2d(in_filters, out_filters, kernel_size=3, padding=1)),
+            nn.BatchNorm2d(out_filters),
+            nn.LeakyReLU(0.2),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
     def forward(self, x):
+        x = x + 0.1 + torch.randn_like(x)
         x = self.conv1(x)
         x = self.conv2(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return F.sigmoid(x)
+        x = self.fc1(x)
+        x = self.disc(x)
+        x = self.fc2(x)
+        return x
+
+
+class MinibatchDiscrimination(nn.Module):
+
+    def __init__(self, in_features, out_features, kernel_dims):
+        super().__init__()
+        self.T = nn.Parameter(torch.randn(in_features, out_features, kernel_dims))
+
+    def forward(self, x):
+        in_features, out_features, kernel_dims = self.T.shape
+        m = x.matmul(self.T.view(in_features, -1))
+        m = m.view(x.shape[0], out_features, kernel_dims)
+        diff = m.unsqueeze(0) - m.unsqueeze(1)
+        norm = torch.sum(torch.abs(diff), dim=3)
+        norm = torch.exp(-norm)
+        features = torch.sum(norm, dim=1)
+        return torch.cat([x, features], dim=1)
