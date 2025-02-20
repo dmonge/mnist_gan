9e5fa46d8f8368de97ec7e8a48bf180917af2fde
David A. Monge <damonge27@gmail.com>
2025-02-20 15:20:12 +0100

    discriminator

--- discriminator.py
+++ discriminator.py
@@ -11,17 +11,16 @@ FAKE_IMAGE = 0.0
 
 class Discriminator(nn.Module):
 
-    def __init__(self, img_size, hidden_size, mbd_in_features=100, mbd_out_features=50, mbd_kernel_dims=5):
+    def __init__(self, img_size, hidden_size, mbd_in_features=100, mbd_out_features=50, mbd_kernel_dims=25):
         super().__init__()
         self.conv1 = self.conv_block(1, hidden_size // 2)
         self.conv2 = self.conv_block(hidden_size // 2, hidden_size)
         self.fc1 = nn.utils.spectral_norm(nn.Linear(hidden_size * img_size // 4 * img_size // 4, mbd_in_features))
-        self.disc = MinibatchDiscrimination(100, mbd_out_features, mbd_kernel_dims)
+        self.disc = MinibatchDiscrimination(mbd_in_features, mbd_out_features, mbd_kernel_dims)
         self.fc2 = nn.Linear(mbd_in_features + mbd_out_features, 1)
 
     def conv_block(self, in_filters, out_filters):
         return nn.Sequential(
-            nn.LeakyReLU(0.2),
             nn.utils.spectral_norm(nn.Conv2d(in_filters, out_filters, kernel_size=3, padding=1)),
             nn.BatchNorm2d(out_filters),
             nn.LeakyReLU(0.2),
